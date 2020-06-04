c4d2cfba8a317641e6dcdb776ede3aff38fba13d
Lucabalduzzi <baldovino.1998@gmail.com>
2020-06-04 15:50:31 +0200

    Classes

---
+++ Es01/Classes/service.py
@@ -0,0 +1,21 @@
+# Classe per la gestione dei Devices
+#
+# Formato json:
+# {
+#   serviceID: "",
+#   description: "",
+#   end_points: {
+#     rest: "",
+#     mqtt: "",
+#   },
+#   timestamp: ""
+# }
+
+class Service(object):
+    
+    def __init__(self, serviceID, description, rest, mqtt=""):
+        self.deviceID = deviceID
+        self.description = description
+        self.end_points['rest'] = rest
+        self.end_points['mqtt'] = mqtt
+        self.timestamp = datetime.datetime.now()
---
+++ Es01/Classes/user.py
@@ -0,0 +1,20 @@
+import json
+
+# Classe per la gestione degli User
+#
+# Formato json:
+# {
+#   userID: "",
+#   name: "",
+#   surname: "",
+#   email: ""
+# }
+
+class User():
+    
+    def __init__(self, userID, name, surname, email):
+        self.userID = userID
+        self.name = name
+        self.surname = surname
+        self.email = email
+        
