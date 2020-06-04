66984a3045459d3a4061d1aa899edae7179efa3c
Lucabalduzzi <baldovino.1998@gmail.com>
2020-06-04 17:31:40 +0200

    Fixes

--- Es01/Classes/device.py
+++ Es01/Classes/device.py
@@ -27,10 +27,6 @@ class Device(object):
     self.end_points['mqtt'] = mqtt
     self.resources = resources
     self.timestamp = datetime.datetime.minute()
-    
-    # Thread
-    self.thread = threading.Thread(target=self.removeDevices)
-    self.thread.start()
   
   def getDeviceID(self):
     return self.id
@@ -55,6 +51,10 @@ class DeviceManager(object):
         self.devices = json.load(file)
         # Non si può dare questo id perchè la lista potrebbe essere più corta dell'id al quale si era arrivati in precedenza
         self.n = len(self.devices)
+    
+    # Thread
+    self.thread = threading.Thread(target=self.removeDevices)
+    self.thread.start()
 
   # Add device
   def addDevice(self, rest, mqtt, resources):
@@ -83,8 +83,8 @@ class DeviceManager(object):
         if datetime.datetime.minute() - device.returnTimestamp < self.TIMEOUT:
           tmp.append(device)
       self.devices = tmp
-      if os.path.exists('./Database/devices.json'):
-        with open('./Database/devices.json', "w") as file:
+      if os.path.exists('../Database/devices.json'):
+        with open('../Database/devices.json', "w") as file:
           json.dump(self.devices)
         
     
