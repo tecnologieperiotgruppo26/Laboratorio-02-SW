9aae3f9b379cbc156ab0937c8a3fb733dd1e8702
Simone <simone.alberto@icloud.com>
2020-06-04 16:03:35 +0200

    init

--- Es01/init.py
+++ Es01/init.py
@@ -15,10 +15,13 @@ class Catalog(object):
   exposed = True
     
   def GET(self, *uri, **params):
-    return open('./freeboard/index.html')
+    if uri[0]=="devices":
+      print("ok")
 
   def POST(self, *uri, **params):
-    if uri[0]=="saveDashboard":
+    if uri[0]=="devices":
+      if uri[1]=="new":
+        print("ok")
       # with open('./freeboard/static/dashboard/dashboard.json', "w") as file:
       #   print(f"{params['json_string']}")
       #   file.write(params['json_string'])
