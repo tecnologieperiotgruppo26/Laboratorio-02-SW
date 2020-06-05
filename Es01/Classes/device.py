7e42c62d89634ebdfde3660a776af66d84ceadd2
Simone <simone.alberto@icloud.com>
2020-06-05 15:20:14 +0200

    fix

--- Es01/Classes/device.py
+++ Es01/Classes/device.py
@@ -52,7 +52,8 @@ class DeviceManager(object):
     # Controllo json
     if os.path.exists('../Database/devices.json'):
       with open('../Database/devices.json') as file:
-        self.devices = json.load(file)
+        self.devices = json.load(file.read())
+        print(file.read())
         # Mantiene consistenza nella numerazione degli elementi
         if len(self.devices):
           self.n = self.devices[-1].returnDeviceID + 1
