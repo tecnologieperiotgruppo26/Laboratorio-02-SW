70f196f6b4220abe10486d7e1d9d14115d6be662
Lucabalduzzi <baldovino.1998@gmail.com>
2020-06-08 17:47:37 +0200

    Modifica Classe Service e fix a Device

--- Es01/Classes/user.py
+++ Es01/Classes/user.py
@@ -16,12 +16,12 @@ import json
 # User object
 ##
 class User():
-    
-    def __init__(self, userID, name, surname, email):
-        self.userID = userID
-        self.name = name
-        self.surname = surname
-        self.email = email
+  
+  def __init__(self, userID, name, surname, email):
+    self.userID = userID
+    self.name = name
+    self.surname = surname
+    self.email = email
 
 ##
 # UserManager object
