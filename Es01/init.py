e81027a31e1350c13578f066fcffde7f77117057
Simone <simone.alberto@icloud.com>
2020-06-08 18:46:34 +0200

    init

--- Es01/init.py
+++ Es01/init.py
@@ -116,9 +116,9 @@ class Catalog(object):
         raise cherrypy.HTTPError(404, "Bad Request!")
     elif uri[0]=="devices" and flag:
       if uri[1]=="new":
-        self.deviceManager.addDevice(time.time(), params['end_points']['rest'],params['resources'],params['end_points']['mqtt'])
+        return self.deviceManager.addDevice(time.time(), params['end_points']['rest'],params['resources'],params['end_points']['mqtt'])
       elif isIDvalid(uri[1]) and int(uri[1]) < self.serviceManager.getNumberOfServices():
-        self.deviceManager.updateDevice(int(uri[1]), time.time())
+        return self.deviceManager.updateDevice(int(uri[1]), time.time())
       else:
         raise cherrypy.HTTPError(404, "Bad Request!")
         
