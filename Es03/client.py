98438b79190d14bb081a24d2e9b57f938418d5b9
Simone <simone.alberto@icloud.com>
2020-06-08 18:39:48 +0200

    client es 3

--- Es03/client.py
+++ Es03/client.py
@@ -14,7 +14,13 @@ if __name__ == "__main__":
   deviceID=-1
   while True:
     print("Client is running")
-
+    if (deviceID==-1):
+      r = requests.post('http://localhost:8080/devices/new')
+      deviceID = int(r.text)
+      print(f"deviceID: {deviceID}")
+    else:
+      r = requests.post(f"http://localhost:8080/devices/{deviceID}")
+      print("Device updated!")
     time.sleep(60)
 
     
